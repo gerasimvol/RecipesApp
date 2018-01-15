ac6547804972245c8ac36c8178cd1b22ccb88e32
ADRABA\vladimir.gerasimenko <vol.gerasimenko@gmail.com>
2018-01-15 15:13:44 +0200

    Finish router

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './../recipe.model';
 import { RecipeService } from './../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,7 +11,10 @@ import { RecipeService } from './../recipe.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[]
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private router: Router,
+              private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes()
@@ -18,5 +22,10 @@ export class RecipeListComponent implements OnInit {
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeService.recipeSelected.emit(recipe)
+  }
+
+  onNewRecipe() {
+    console.log(this.route)
+    // this.router.navigate(['new'], {relativeTo: this.route})
   }
 }
